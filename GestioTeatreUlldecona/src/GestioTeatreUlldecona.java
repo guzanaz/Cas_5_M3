f4bf9ce536955002337342f8b83876d7f64fa4a8
Daniela <daniela.gallardo.reyes@gmail.com>
2021-03-16 17:31:42 +0100

    update

--- GestioTeatreUlldecona/src/GestioTeatreUlldecona.java
+++ GestioTeatreUlldecona/src/GestioTeatreUlldecona.java
@@ -1,15 +1,14 @@
+
 /**
- * Cas_5 M03
- * Programa GestioTeatreUlldecona: 
- * Programa inicial del proyecto del mismo nombre.
- * Implementa una aplicación para la gestión del teatro (obras, público, patio de butacas).
- * Llama a las clases Clients.java, Teatre.java, Obra.java y Seient.java
- *  
+ * Cas_5 M03 Programa GestioTeatreUlldecona: Programa inicial del proyecto del
+ * mismo nombre. Implementa una aplicación para la gestión del teatro (obras,
+ * público, patio de butacas). Llama a las clases Clients.java, Teatre.java,
+ * Obra.java y Seient.java
+ * 
  * @author Daniela Gallardo Reyes
  * @version 1.0
- * @since   15-03-2021
-*/
-
+ * @since 15-03-2021
+ */
 
 public class GestioTeatreUlldecona {
 
