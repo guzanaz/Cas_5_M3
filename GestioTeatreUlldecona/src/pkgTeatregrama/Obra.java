a56b05d1871ebc4e2d4e8f3d4433e89c6404d40b
Daniela <daniela.gallardo.reyes@gmail.com>
2021-03-17 12:49:43 +0100

    update clases pkgTeatregrama
    comentario inicial en clases

--- GestioTeatreUlldecona/src/pkgTeatregrama/Obra.java
+++ GestioTeatreUlldecona/src/pkgTeatregrama/Obra.java
@@ -1,5 +1,16 @@
 package pkgTeatregrama;
 
+/**
+ * Cas_5 M03 Clase Obra: clase del proyecto GestioTeatreUlldecona. Conforma
+ * el paquete pkgTeatregrama. Contiene atributos y métodos para la gestión de
+ * las obras del teatro. Sus atributos y métodos se utilizan en el programa
+ * inicial GestioTeatreUlldecona.java
+ * 
+ * @author Daniela Gallardo Reyes
+ * @version 1.0 (Esqueleto de la clase)
+ * @since 15-03-2021
+ */
+
 public class Obra {
 	/**
 	 * main sólo para pruebas debe ser borrado en su versión final
--- GestioTeatreUlldecona/src/pkgTeatregrama/Teatre.java
+++ GestioTeatreUlldecona/src/pkgTeatregrama/Teatre.java
@@ -1,5 +1,16 @@
 package pkgTeatregrama;
 
+/**
+ * Cas_5 M03 Clase Teatre: clase del proyecto GestioTeatreUlldecona. 
+ * Conforma el paquete pkgTeatregrama. 
+ * -
+ * sus recursos se ejecutan en el programa inicial GestioTeatreUlldecona.java
+ * 
+ * @author Daniela Gallardo Reyes
+ * @version 1.0 (Esqueleto de la clase)
+ * @since 15-03-2021
+ */
+
 public class Teatre {
 	/**
 	 * main sólo para pruebas debe ser borrado en su versión final
